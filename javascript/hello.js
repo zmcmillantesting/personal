8530811787295422f3e37c91aad19104a56bc88f
Zach McMillan <zmcmillan@PCB-solutions.local>
2025-04-30 08:16:33 -0600

    checking to see if push works in a different IDE

--- javascript/hello.js
+++ javascript/hello.js
@@ -1,1 +1,12 @@
-console.log("Hello, World!");
+const prompt = require("prompt-sync")();
+
+let statement = prompt("Please enter a statement: ");
+print_Statement(statement);
+
+function print_Statement(statement) {
+  if (statement !== "") {
+    console.log(statement);
+  } else {
+    console.log("The statement is blank");
+  }
+}
